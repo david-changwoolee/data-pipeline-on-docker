04d30c26c99ff026ceade3d92ca22ea0502cfb92
eyeballss <kkvv1111@gmail.com>
2025-07-24 18:49:50 +0900

    use mysql as hive metastore

--- hadoop/entrypoint.sh
+++ hadoop/entrypoint.sh
@@ -9,8 +9,8 @@ hdfs dfs -chown hive:hdfs /user/hive/warehouse
 hdfs dfs -chmod 775 /user/hive/warehouse
 
 #hive entrypoint
-#schematool -dbType mysql -initSchema
-schematool -dbType derby -initSchema
+schematool -dbType mysql -initSchema
+#schematool -dbType derby -initSchema
 nohup hive --service metastore &> ${HIVE_HOME}/logs/metastore.log &
 
 exec "$@"
